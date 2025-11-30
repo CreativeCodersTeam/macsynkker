d3afcde1cf3b7e9b8a5b863dc43716f25e39224d
darthsharp <48331467+darthsharp@users.noreply.github.com>
2025-11-30 16:32:38 +0100

    Set up initial project structure with solution files and configuration files
    - Added `.editorconfig` for consistent code styles.
    - Added `.gitattributes` for proper line endings and merge conflict resolution.
    - Initialized `.gitignore` for Visual Studio and .NET-specific files.
    - Created `global.json` targeting .NET SDK 10.0.100.
    - Added a Visual Studio solution (`macSynkker.sln`) with project folders.
    - Implemented a basic console application project (`CreativeCoders.MacSynkker.Cli`).

---
+++ source/CreativeCoders.MacSynkker.Cli/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.Versioning;
+
+namespace CreativeCoders.MacSynkker.Cli;
+
+internal static class Program
+{
+    [SupportedOSPlatform( "macos")]
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+    }
+}
